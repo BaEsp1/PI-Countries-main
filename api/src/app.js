05db343a990d2e14db598d16d60197d1d93adbb3
Bárbara Espinola <Baesp2023@gmail.com>
2023-12-07 00:01:50 -0300

    update CORSS

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-country-phi.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-country-baesp1.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
