2c07b9c240c100a824ec8790d0d2cb299ac7f644
Bárbara Espinola <Baesp2023@gmail.com>
2023-12-06 22:03:00 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,26 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {getData} = require('./src/controllers/saveData.js');
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 console.log('Iniciando la aplicación');
 conn.sync()
   .then(() => {
     console.log('Base de datos sincronizada');
-    server.listen(3001, async () => {
+    server.listen(port, async () => {
       console.log('Servidor ON');
       getData();
+      server.get("/", (req, res) => {
+        const htmlResponse = `
+          <html>
+            <body>
+              <h1>Soy un proyecto Back end en vercel</h1>
+            </body>
+          </html>
+        `;
+        res.send(htmlResponse);
+      });
     });
   })
   .catch((error) => {
